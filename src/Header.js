0c7216debffe4e4292a3b1c4848e7f0b9b7feb0c
Caroline428 <sasagricenko916@gmail.com>
2022-11-22 01:33:22 +0300

    [ITKAMASUTRA-2] Начало создания основного каркаса проекта

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = ()  => {
+    return (
+        <div>
+            <a href="#s">Home</a>
+            <a href="#s">News Feed</a>
+            <a href="#s">Еще чето</a>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/Technologies.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Technologies = () => (
+    <div>
+        <ul>
+            <li>вв</li>
+            <li>ппп</li>
+            <li>ннн</li>
+            <li>ррррр</li>
+        </ul>
+    </div>
+)
+
+export default Technologies
