bfa1d5384c9570402e9d65db32339d8f705c92ca
Caroline428 <sasagricenko916@gmail.com>
2022-12-19 16:30:09 +0300

    [WOTS-9] Сверстана страница Dialogs

--- src/components/pages/Dialogs/Dialogs.jsx
+++ src/components/pages/Dialogs/Dialogs.jsx
@@ -1,7 +1,22 @@
 import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
-    return (<div>Диалоги</div>)
+    return (
+        <div className={s.Dialogs}>
+            <div className={s.Dialogs__Items}>
+                <div className={s.Dialog}>Катя</div>
+                <div className={s.Dialog}>Маша</div>
+                <div className={s.Dialog}>Федя</div>
+                <div className={s.Dialog}>Виктор</div>
+            </div>
+            <div className={s.Messages__Items}>
+                <div className={s.Message}>Ололол</div>
+                <div className={s.Message}>Трололо</div>
+                <div className={s.Message}>Привет</div>
+                <div className={s.Message}>Как дела</div>
+            </div>
+        </div>
+    )
 }
 
 export default Dialogs
